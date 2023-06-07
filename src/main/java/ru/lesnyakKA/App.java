13cfbdf09035b36285cf61b99cfb1d0feff69d85
lesnyakKa <lesnyakKA@yandex.ru>
2023-06-07 21:11:59 +0300

    Refactoring with stream solution.

--- src/main/java/ru/lesnyakKA/App.java
+++ src/main/java/ru/lesnyakKA/App.java
@@ -1,21 +1,11 @@
 package ru.lesnyakKA;
 
 import java.util.Arrays;
+
 import org.apache.commons.lang3.ArrayUtils;
 
 class App {
-    public static int countUniqChars(String text) {
-        char[] chars = text.toCharArray();
-        char[] result = new char[chars.length];
-        int index = 0;
-
-        for (int i = 0; i < chars.length; i++) {
-            if (ArrayUtils.contains(chars, chars[i]) &&
-                    !ArrayUtils.contains(result, chars[i])) {
-                result[index] = chars[i];
-                index++;
-            }
-        }
-        return index;
+    public static long countUniqChars(String text) {
+        return text.chars().distinct().count();
     }
 }
