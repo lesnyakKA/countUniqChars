d4a0321a67690a2a9de175df9ba76941466ffaf6
lesnyakKa <lesnyakKA@yandex.ru>
2023-06-07 21:13:23 +0300

    Refactoring with stream solution.

--- src/main/java/ru/lesnyakKA/App.java
+++ src/main/java/ru/lesnyakKA/App.java
@@ -1,9 +1,5 @@
 package ru.lesnyakKA;
 
-import java.util.Arrays;
-
-import org.apache.commons.lang3.ArrayUtils;
-
 class App {
     public static long countUniqChars(String text) {
         return text.chars().distinct().count();
